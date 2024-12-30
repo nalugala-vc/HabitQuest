53111db201c0b3fabdf3c9f21c93682b3861e3ba
nalugala_vc <vanessachebukwa@gmail.com>
2024-12-30 16:18:00 +0300

    Flutter firebase setup

--- solutech/lib/main.dart
+++ solutech/lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCDiz5XZsR9FbxWH1EQ21KCVxR850S5sWE",
+            authDomain: "habitquest-b5a20.firebaseapp.com",
+            projectId: "habitquest-b5a20",
+            storageBucket: "habitquest-b5a20.firebasestorage.app",
+            messagingSenderId: "260752576315",
+            appId: "1:260752576315:web:094bd759253fc68a05f11e"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
